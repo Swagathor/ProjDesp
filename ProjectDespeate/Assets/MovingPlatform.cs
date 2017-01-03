deecf914edc965510c46692aea8f22aaf9a5dda3
Swagathor <adam.wrange@gmail.com>
2017-01-03 21:34:20 +0100

    WIP Moving platform

--- ProjectDespeate/Assets/MovingPlatform.cs
+++ ProjectDespeate/Assets/MovingPlatform.cs
@@ -8,20 +8,23 @@ public class MovingPlatform : MonoBehaviour {
     public enum CyckleMethod    {  bounce, repeat    }
     
     public Transform[] m_Waypoints;
-    public float moveSpeed;
+    public float moveSpeed = 5f; 
     public bool loop;
 
     private Vector3 m_currentPosition;
+    private int currentWaypoint = 0;
 
     //Inituialization
 	void Start ()
     {
-	
+        
+        GoToNextPoint();
 	}
 	
 
 	void Update ()
     {
+        
         if (loop == true)
             Loop();
         else
@@ -36,7 +39,20 @@ public class MovingPlatform : MonoBehaviour {
     //Looping
     void Loop()
     {
-        transform.position = Vector3.Lerp(m_currentPosition, m_Waypoints, Time.Deltatime * moveSpeed);
 
+        m_currentPosition = Vector3.MoveTowards(m_currentPosition, m_Waypoints[currentWaypoint].position, moveSpeed);
+        float dist = Vector3.Distance(m_currentPosition, m_Waypoints[currentWaypoint].position);
+        if (dist < 0.2f)
+            GoToNextPoint();
+        
+    }
+    void GoToNextPoint()
+    {
+        if (m_Waypoints.Length == 0)
+        {
+            return;
+        }
+
+        currentWaypoint = (currentWaypoint + 1) % m_Waypoints.Length;
     }
 }
